fb3fd6969ee88cc8416b0f625857065074179e76
Henok Mekonnen <henatan99@yahoo.com>
2021-10-13 14:33:54 +0300

    Add App class with initialize method in main.rb

--- main.rb
+++ main.rb
@@ -1,3 +1,11 @@
+class App()
+    def initialize()
+        @books = []
+        @people = []
+        @rentals = []    
+    end
+end 
+
 def main
     app = App.new()
     puts "Welcome to school library app \n\n"
